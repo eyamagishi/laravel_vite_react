9efdb6b8ea564e2282382007657e37f2ee080560
eyamagishi <e.yamagishi0625@gmail.com>
2024-06-06 23:25:17 +0900

    add: 画像の表示

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,11 +2,15 @@ import "./bootstrap";
 
 import ReactDOM from "react-dom/client";
 import Counter from "./components/Counter";
+import Logo from "../images/logo-with-shadow.png";
 
 function App() {
     return (
         <>
             <h1>Hello World</h1>
+            <img src={Logo} />
+            {/* publicフォルダに画像を配置したとき */}
+            {/* <img src="/logo-with-shadow.png" /> */}
             <Counter />
         </>
     );
