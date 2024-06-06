47d8a11763e902c05e1bdbe81c102f672aa9d70f
eyamagishi <e.yamagishi0625@gmail.com>
2024-06-06 23:20:44 +0900

    add: コンポーネントの作成

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,15 @@
 import "./bootstrap";
 
 import ReactDOM from "react-dom/client";
+import Counter from "./components/Counter";
 
 function App() {
-    return <h1>Hello World</h1>;
+    return (
+        <>
+            <h1>Hello World</h1>
+            <Counter />
+        </>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
